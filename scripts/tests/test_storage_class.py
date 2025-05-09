526df58bd13dbee9516322044ca534e301da6c3f
= <yuda.pinhas@skai.io>
2025-05-09 16:45:34 +0300

    charts/cleanup

--- scripts/tests/test_storage_class.py
+++
@@ -1,8 +0,0 @@
-from scripts.entity_collections import StorageClasses
-from scripts.tests.constants import STORAGE_CLASSES
-
-class TestStorageClass:
-    def test_storage_class_exists(self):
-        storage_classes = StorageClasses(namespace=None).get()
-
-        assert any(sc.name() in STORAGE_CLASSES for sc in storage_classes)
