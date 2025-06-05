49aea257649380e2f569493f5651427c440513c9
yudap <yuda691@gmail.com>
2025-06-05 17:49:45 +0100

    jenkins/newfeat-terragrunt-cicd

--- charts/jenkins/jenkins-dsl/repos.groovy
+++ charts/jenkins/jenkins-dsl/repos.groovy
@@ -3,5 +3,10 @@
         org:    'yudapinhas',
         name:   'netgod-terraform',
         sshUrl: 'git@github.com:yudapinhas/netgod-terraform.git'
+    ],
+    [
+        org:    'yudapinhas',
+        name:   'netgod-terragrunt',
+        sshUrl: 'git@github.com:yudapinhas/netgod-terragrunt.git'
     ]
 ]
